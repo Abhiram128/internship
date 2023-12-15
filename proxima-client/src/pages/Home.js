9ee00d3f0f7ded398be740ba39b92d59bb8d4897
Abhiram128 <90932429+Abhiram128@users.noreply.github.com>
2023-12-15 22:04:03 +0530

    Update Home.js

--- proxima-client/src/pages/Home.js
+++ proxima-client/src/pages/Home.js
@@ -1,57 +1,50 @@
-  import { useEffect } from "react";
-  import ProjectDetails from "../components/ProjectDetails";
-  import ProjectForm from "../components/ProjectForm";
-  import { useAuthContext } from "../hooks/useAuthContext";
-  import { useProjectsContext } from "../hooks/useProjectsContext";
+import { useEffect } from "react";
+import ProjectDetails from "../components/ProjectDetails";
+import ProjectForm from "../components/ProjectForm";
+import { useAuthContext } from "../hooks/useAuthContext";
+import { useProjectsContext } from "../hooks/useProjectsContext";
 
-  const Home = () => {
-    const { projects, dispatch } = useProjectsContext();
-    const { user } = useAuthContext();
+const Home = () => {
+  const { projects, dispatch } = useProjectsContext();
+  const { user } = useAuthContext();
 
-    useEffect(() => {
-      const getAllProjects = async () => {
-        const res = await fetch(
-          `${process.env.REACT_APP_BASE_URL}/api/projects`,
-          {
-            headers: {
-              Authorization: `Bearer ${user.token}`,
-            },
-          }
-        );
-        const json = await res.json();
-
-        if (res.ok) {
-          dispatch({ type: "SET_PROJECTS", payload: json });
+  useEffect(() => {
+    const getAllProjects = async () => {
+      const res = await fetch(
+        `${process.env.REACT_APP_BASE_URL}/api/projects`,
+        {
+          headers: {
+            Authorization: `Bearer ${user.token}`,
+          },
         }
-      };
+      );
+      const json = await res.json();
 
-      if (user) {
-        getAllProjects();
+      if (res.ok) {
+        dispatch({ type: "SET_PROJECTS", payload: json });
       }
-    }, [dispatch, user]);
+    };
 
-    return (
-      <div className='home container mx-auto py-10 grid md:grid-cols-3 gap-10 w-full'>
-        <div className='left md:col-span-2'>
+    if (user) {
+      getAllProjects();
+    }
+  }, [dispatch, user]);
+
+  return (
+    <div className='home container mx-auto py-10 min-h-screen grid gap-10 grid-cols-1 md:grid-cols-2'>
+      {projects.length < 1 ? (
+        <div className='left'>
           <h2 className='text-3xl text-sky-400 mb-10'>
-            {projects.length < 1 ? (
-              "No project available here!"
-            ) : (
-              <span>
-                {projects.length === 1 ? "Your Project" : "Your all projects"}
-              </span>
-            )}
+            No project available here!
           </h2>
-          <div className='projects-wrapper flex gap-10 flex-wrap'>
-            {projects &&
-              projects.map((project) => (
-                <ProjectDetails key={project._id} project={project} />
-              ))}
-          </div>
         </div>
-        <ProjectForm />
-      </div>
-    );
-  };
+      ) : (
+        projects.map((project) => (
+          <ProjectDetails key={project._id} project={project} />
+        ))
+      )}
+    </div>
+  );
+};
 
-  export default Home;
+export default Home;
